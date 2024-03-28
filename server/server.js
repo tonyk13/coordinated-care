2cc77e28cf2aca319ba6b2116295f84883a772dd
Tony Kareeparampil <tony.kareeparampil@stonybrook.edu>
2024-03-28 00:22:05 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -63,6 +63,7 @@ app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
+const path = require("path");
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
