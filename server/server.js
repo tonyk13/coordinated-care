9e11281d6e14b397dabcba0425e974d55da7a338
Tony Kareeparampil <tony.kareeparampil@stonybrook.edu>
2024-03-27 23:53:07 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -61,12 +61,12 @@ db.once("open", () => {
 // Routes
 if (process.env.NODE_ENV === "production") {
 	// Exprees will serve up production assets
-	app.use(express.static("client/build"));
+	app.use(express.static("client/public"));
 
 	// Express serve up index.html file if it doesn't recognize route
 	const path = require("path");
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+		res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
 	});
 }
 app.use("/api", answersRoutes);
