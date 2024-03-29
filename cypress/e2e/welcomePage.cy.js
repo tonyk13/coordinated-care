f5b423c470223e9189c304f3c6c8eaaa45a34ce3
lopez012 <122379590+lopez012@users.noreply.github.com>
2024-03-29 11:06:04 -0400

    front-end tests

---
+++ cypress/e2e/welcomePage.cy.js
@@ -0,0 +1,14 @@
+describe('Welcome Page', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('https://coordinated-care-cce88007d728.herokuapp.com')
+  })
+  it('should render the Continue as Guest button', () => {
+    cy.get('.guestButton').should('exist');
+    cy.get('.guestButton').contains('Continue as Guest');
+  });
+
+})
