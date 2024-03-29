ef84863790092f53f8596bdfcd4801215ee0bdae
vinair2 <vinair22@gmail.com>
2024-03-29 16:13:52 -0400

    Update WelcomePage.js

--- client/src/components/WelcomePage/WelcomePage.js
+++ client/src/components/WelcomePage/WelcomePage.js
@@ -20,7 +20,7 @@ function WelcomePage({ setCurrentPage, setIsLoggedIn, isGuest, setIsGuest, isOnl
 		return (
 			<div id="welcomePage">
 				<h1>Fake Stack Overflow</h1>
-				<h2>Got questions? We have the answers.</h2>
+				<h2>TEsting Functionality</h2>
 				<WelcomePageBox
 					isLoggingIn={isLoggingIn}
 					setIsLoggingIn={setIsLoggingIn}
