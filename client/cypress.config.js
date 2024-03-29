126c309ca677a68b454f212c9946b79819d8d31e
lopez012 <122379590+lopez012@users.noreply.github.com>
2024-03-28 23:52:23 -0400

    boiler plate for cypress

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'x8vfgc',
   component: {
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
