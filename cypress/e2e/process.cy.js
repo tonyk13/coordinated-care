ed8182f00bc0bf6dac059c9f5e07b2703a2801f3
vinair2 <vinair22@gmail.com>
2024-04-12 15:00:46 -0400

    Create process.cy.js

---
+++ cypress/e2e/process.cy.js
@@ -0,0 +1,20 @@
+describe('Processes Screen', () => {
+    beforeEach(() => {
+
+      cy.visit('https://coordinated-care-cce88007d728.herokuapp.com')
+    })
+    it('should render table when processes is clicked', () => {
+      cy.contains('.MuiListItemText-primary', 'processes').click();
+      cy.get('#questionsButton').should('be.visible');
+      
+      it('should verify Searchbar element is visible', () => {
+        cy.get('input#search').should('be.visible', { timeout: 5000 });
+      });
+    
+      it('should verify Table element is visible', () => {
+        cy.get('[component="Paper"]').should('be.visible', { timeout: 5000 });
+      });
+        
+    });
+  
+  })
