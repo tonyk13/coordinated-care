1e0f71b855dbced05013df600e37adf3e07a45dd
Tony Kareeparampil <tony.kareeparampil@stonybrook.edu>
2024-04-17 23:31:11 -0400

    problem code

--- server/server.js
+++ server/server.js
@@ -83,10 +83,6 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "..", "client", "build"));
-});
-
 // app.get("*", (req, res) => {
 // 	res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 // });
