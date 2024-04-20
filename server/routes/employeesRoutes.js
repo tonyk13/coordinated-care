6c16adba94f3714998af8497b6405960c1f68392
vinair2 <vinair22@gmail.com>
2024-04-20 00:07:11 -0400

    Update Personal Information

--- server/routes/employeesRoutes.js
+++ server/routes/employeesRoutes.js
@@ -11,13 +11,15 @@ router.get("/employees/all_physician_names", employeesController.all_physician_n
 // // POST request for creating an employee
 // router.post("/employees/", employeesController.create_employee);
 
-// // GET request for retrieving an employee
-// router.get("/employees/:_id", employeesController.get_employee);
+// GET request for retrieving an employee
+router.get("/employees/:_id", employeesController.get_employee);
 
-// Get request for retrieving an employee id by email (used when setting cookie
+// PUT request for updataing an employee
+router.put("/employees/:_id", employeesController.update_employee);
+
+
+// Get request for retrieving an employee id by email (used when setting cookie)
 router.post("/employees/get_employee_by_email", employeesController.get_employee_by_email);
 
-// // PUT request for updating an employee
-// router.put("/employees/:_id", employeesController.update_employee);
 
 module.exports = router;
