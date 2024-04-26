1892baa1f661d3c2a60abdf2079bd01b5e915343
Tony Kareeparampil <tony.kareeparampil@stonybrook.edu>
2024-04-26 15:44:34 -0400

    all patients e2e test

---
+++ cypress/e2e/AllPatients.cy.js
@@ -0,0 +1,14 @@
+describe("All Patients", () => {
+	beforeEach(() => {
+		cy.login();
+	});
+	it("All Patients", () => {
+		cy.get("#continueAsAdmintempButton").click();
+
+		cy.get(".Patients").click();
+
+		cy.contains("button", "Add new Patient").should("be.visible");
+
+		cy.get("#search").type("search");
+	});
+});
