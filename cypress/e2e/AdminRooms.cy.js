c57583177441474c580443843b60aeafac4246df
Tony Kareeparampil <tony.kareeparampil@stonybrook.edu>
2024-05-05 13:41:07 -0400

    fixed rooms test

--- cypress/e2e/AdminRooms.cy.js
+++ cypress/e2e/AdminRooms.cy.js
@@ -3,10 +3,21 @@ describe("Admin Rooms", () => {
 		cy.login();
 	});
 	it("Admin Rooms", () => {
-		// cy.get('#continueAsAdmintempButton').click();
 		cy.get(".Rooms").click();
-		cy.contains("button", "Request").should("be.visible");
-		cy.contains("button", "Edit").should("be.visible");
-		cy.contains("button", "Cancel").should("be.visible");
+
+		cy.get("h2").contains("Rooms").should("be.visible");
+
+		cy.get('input[type="search"]').should("have.id", "search");
+		cy.get("#search").should("be.visible");
+
+		cy.get("table").should("be.visible");
+		cy.get("thead > tr").within(() => {
+			cy.get("th").eq(0).should("contain", "Room Number");
+			cy.get("th").eq(1).should("contain", "Status");
+			cy.get("th").eq(2).should("contain", "Notes");
+			cy.get("th").eq(3).should("contain", "Next Available");
+		});
+
+		cy.get("tbody > tr").its("length").should("be.gt", 0);
 	});
 });
